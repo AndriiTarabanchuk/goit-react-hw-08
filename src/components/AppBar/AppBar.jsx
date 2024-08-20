48d974b2bd174721faeda2881b6c3402f2f7107a
AndreyTara <explorituse@gmail.com>
2024-08-20 11:09:10 +0300

    'adds-userEmail'

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,15 +1,18 @@
+import { useSelector } from "react-redux";
 import AuthNav from "../AuthNav/AuthNav";
 import Navigation from "../Navigation/Navigation";
 import UserMenu from "../UserMenu/UserMenu";
 import css from "./AppBar.module.css";
 import React from "react";
+import { selectUser } from "../../redux/auth/selectors";
 
 const AppBar = () => {
+  const user = useSelector(selectUser);
   return (
     <div className={css.wrap}>
       <div>
         <p>
-          Welcome, <span>user.email</span>
+          Welcome, <span>{user.email}</span>
         </p>
       </div>
       <nav className={css.nav}>
