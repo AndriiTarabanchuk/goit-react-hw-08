df037cd5ddbb8138effb0cbc057e48991d806f4d
AndreyTara <explorituse@gmail.com>
2024-08-23 14:03:40 +0300

    'fix-AppBar-change-userMenu'

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,18 +2,27 @@ import { NavLink } from "react-router-dom";
 import css from "./UserMenu.module.css";
 import React from "react";
 import { logoutThunk } from "../../redux/auth/operations";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "../../redux/auth/selectors";
 
 const UserMenu = () => {
+  const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
   return (
     <>
-      <li className={css.item}>
+      <li>
         <NavLink to="/contacts">Contacts</NavLink>
       </li>
-      <li>
+      <li className={css.box}>
         {/* <NavLink to="/">Log out</NavLink> */}
+        <div className={css.userName}>
+          {user?.email && (
+            <i>
+              Welcome, <span>{user.email}</span>!
+            </i>
+          )}
+        </div>
         <button className={css.btn} onClick={() => dispatch(logoutThunk())}>
           Logout
         </button>
